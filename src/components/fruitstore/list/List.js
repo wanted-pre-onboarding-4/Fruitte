cb5798c32299db10b1c77160e52071f8970e6942
vjvl95 <vjvl95@naver.com>
2022-09-03 21:38:59 +0900

    feat : 페이지네이션 구현

--- src/components/fruitstore/list/List.js
+++ src/components/fruitstore/list/List.js
@@ -1,10 +1,41 @@
-import { useEffect } from 'react';
-import productAtom from '../../../store/productAtom';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import productAtom from '../../../store/productAtom';
+import Pagination from './Pagenation';
 export default function List() {
+  const product = useRecoilValue(productAtom);
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * 10;
+  useEffect(() => {
+    console.log(product);
+  });
   return (
-    <div
-      style={{ width: '280px', height: '300px', border: '2px solid black', borderRadius: '50px' }}
-    ></div>
+    <>
+      <Container>
+        {product.slice(offset, offset + 10).map((pro, index) => {
+          return <Card>{pro.product_id}</Card>;
+        })}
+      </Container>
+      <Pagination total={product.length} limit={10} page={page} setPage={setPage} />
+    </>
   );
 }
+
+const Container = styled.div`
+  width: 1500px;
+  height: 700px;
+  display: grid;
+  grid-template-columns: repeat(5, 300px);
+  row-gap: 50px;
+  column-gap: 20px;
+  justify-items: center;
+  margin: 50px auto;
+`;
+
+const Card = styled.div`
+  width: 280px;
+  height: 300px;
+  border: 2px solid black;
+  border-radius: 50px;
+`;
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,6 +1,3 @@
-import { useEffect } from 'react';
-import productAtom from '../store/productAtom';
-import { useRecoilValue } from 'recoil';
 import List from '../components/fruitstore/list/List';
 export default function ListPage() {
   const product = useRecoilValue(productAtom);
@@ -8,27 +5,5 @@ export default function ListPage() {
   useEffect(() => {
     console.log(product);
   });
-  return (
-    <div
-      style={{
-        width: '1500px',
-        height: '700px',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(5, 300px)',
-        rowGap: '50px',
-        columnGap: '20px',
-        justifyItems: 'center',
-        margin: '50px auto',
-      }}
-    >
-      {product.map((pro, index) => {
-        if (index >= 10) {
-          console.log(pro);
-        } else {
-          console.log(index);
-          return <List />;
-        }
-      })}
-    </div>
-  );
+  return <List />;
 }
