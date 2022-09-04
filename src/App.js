fed427f0d028172e495fdd0bb63966e24c8e338c
Jaehyeok <milkboy2564@naver.com>
2022-09-04 20:49:09 +0900

    fix: debugging용 코드 제거

--- src/App.js
+++ src/App.js
@@ -29,17 +29,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<div>Root</div>} />
-        <Route path="/fruitstore" element={<div>상품 전체 조회 페이지</div>} />
         <Route path="/fruitstore/:product_id" element={<FruitStoreDetailPage />} />
         <Route path="/fruitstore" element={<ListPage />} />
         <Route path="/fruitstore/:product_id" element={<div>상품 상세 페이지</div>} />
         <Route path="/shop_payment/:product_id" element={<div>상품 주문 페이지</div>} />
-        <Route path="/shop_payment/complete" element={<div>주문 내역 확인 페이지</div>} />
         <Route path="/admin" element={<div>관리자 등록페이지</div>} />
         <Route path="/admin/management" element={<AuthManagement />} />
         <Route path="/shop_payment/complete" element={<PaymentComplet />} />
-
-        <Route path="/admin" element={<div>관리자 페이지</div>} />
       </Routes>
     </BrowserRouter>
   );
