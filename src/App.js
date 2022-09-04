6e9c988ea34df02abc8d034171e70f07d3c0a010
이시형 <tlgud585@gmail.com>
2022-09-04 19:09:30 +0900

    feet: order page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/commons/Header';
 import productAtom from './store/productAtom';
 import GlobalStyle from './styles/Globalstyles';
 import ProductData from './data/product.json';
+import Order from './pages/order/Order';
 
 function App() {
   const setProductData = useSetRecoilState(productAtom);
@@ -21,7 +22,7 @@ function App() {
         <Route path="/" element={<div>Root</div>} />
         <Route path="/fruitstore" element={<div>상품 전체 조회 페이지</div>} />
         <Route path="/fruitstore/:product_id" element={<div>상품 상세 페이지</div>} />
-        <Route path="/shop_payment/:product_id" element={<div>상품 주문 페이지</div>} />
+        <Route path="/shop_payment/:product_id" element={<Order/>} />
         <Route path="/shop_payment/complete" element={<div>주문 내역 확인 페이지</div>} />
         <Route path="/admin" element={<div>관리자 페이지</div>} />
       </Routes>
---
+++ src/pages/order/Order.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import OrderList from '../../components/fruitstore/order/OrderList';
+import OrderInfo from '../../components/fruitstore/order/OrderInfo';
+const DUMMY_PRODUCT = {
+  order_id: 1,
+  product_image: 'https://cdn.imweb.me/thumbnail/20200715/53fa50226803f.jpg',
+  product_name: '슈퍼콜라겐 에센스 28일',
+  product_amount: 1,
+  product_price: 96000,
+  discount_rate: 1000,
+  delivery_fee : 1000
+};
+
+const Order = () => {
+  return (
+    <Main>
+      <OrderList data={DUMMY_PRODUCT}/>
+      <OrderInfo data={DUMMY_PRODUCT}/>
+    </Main>
+  );
+};
+
+export default Order;
+
+const Main = styled.main`
+  padding: 3em 1em;
+  width: 100%;
+  @media screen and (min-width: 1280px) {
+    max-width: 1280px;
+    margin: 0 auto;
+  }
+`;
